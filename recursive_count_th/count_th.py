cd932b5a80282e7b2e947701d45c0da5545383c5
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-06-14 21:07:49 -0500

    Count_Th Complete without using for loops all tests pass.

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,15 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    #We need to check if it is less than 2 letters or if nothing is entered
+    if len(word) < 2 or len(word) == 0:
+        #This cannot be none
+        return 0
+    #This slices these 2 letters
+    elif word[:2] == "th":
+        #here we are goig to return the occurrense recursively
+        #we must increment by 1
+        return count_th(word[1:]) + 1
+    else:
+        #otherwise return the occurrences and be done.
+        return count_th(word[1:])
